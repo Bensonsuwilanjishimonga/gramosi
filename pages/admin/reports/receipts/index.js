6c956d4a575a1d027ae336d479b9124d0c5a9487
Imuks <bshimonga2@gmail.com>
2021-06-12 10:43:58 +0200

    added a sales receipt from masongo patrick

--- pages/admin/reports/receipts/index.js
+++ pages/admin/reports/receipts/index.js
@@ -1,13 +1,81 @@
-import { Descriptions, Badge } from 'antd';
+import { Descriptions, Badge, Row, Col, Space, Divider } from 'antd';
+import { Input } from 'antd';
+
 
 const Receipt = () => {
+
+
+
+const renderDetails  = ()=>{
+
+
+
+  return   <Col  span={12} >
+    <Row gutter={[24,24]}>
+
+    <Col span={12}>
+  <div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Date</div>
+    
+    </Col>
+    <Col span={12}>
+  <div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>08/April/2021</div>
+    
+    </Col>
+
+      </Row>
+
+      <Row gutter={[16,16]}>
+
+
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Receipt No.</div>
+
+</Col>
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>1325</div>
+
+</Col>
+
+  </Row>
+
+  <Row gutter={[16,16]}>
+
+
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Cheque No.</div>
+
+</Col>
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}></div>
+
+</Col>
+
+  </Row>
+
+
+
+
+
+
+</Col>
+
+
+
+}
+
+
+
+
+
     return (
 
-<Descriptions title="RECEIPT" bordered>
-    <Descriptions.Item label="DATE: 08/April/2021"></Descriptions.Item>
-    <Descriptions.Item label=""></Descriptions.Item>
-    <Descriptions.Item label="">MWANSABOMBWE TOWN COUNCIL - ECZ
-    <br/>
+
+      <>
+
+<Row gutter={[16, 16]}>
+  <Col  span={24} >
+      <div style={{backgroundColor:'white',height:150, marginTop:20, fontWeight:'bold',color:'green' }}>MWANSABOMBWE TOWN COUNCIL - ECZ
+      <br/>
 P.O Box:
 750002
 <br/>
@@ -17,37 +85,108 @@ Phone No.:
 <br/>
 
 Physical Address: MWANSABOMBWE
-District:
-MWANSABOMBWE
+<br/>
+District: MWANSABOMBWE
 <br/>
 
-</Descriptions.Item>
-    <Descriptions.Item label=" Receipt No: 1325"></Descriptions.Item>
-    <Descriptions.Item label="" span={2}>
-Received from      <br/>
-MASONGO PATSON<br/>
-    </Descriptions.Item>
-    <Descriptions.Item label="Description:" span={3}>
-    </Descriptions.Item>
-    <Descriptions.Item label="RENTALS FROM HOUSES">$80.00</Descriptions.Item>
-    <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
-    <Descriptions.Item label="Official Receipts">$60.00</Descriptions.Item>
-    <Descriptions.Item label="Config Info">
-      Data disk type: MongoDB
-      <br />
-      Database version: 3.4
-      <br />
-      Package: dds.mongo.mid
-      <br />
-      Storage space: 10 GB
-      <br />
-      Replication factor: 3
-      <br />
-      Region: East China 1<br />
-    </Descriptions.Item>
-  </Descriptions>
-  
-  );
-    };
+</div>
+    </Col>
+    </Row>
+
+
+<Row gutter={[16, 16]}>
+  <Col  span={12} >
+      <div style={{backgroundColor:'blue',height:190, textAlign:'center', marginTop:15,fontSize:20, fontWeight:'bold'}}>
+        Received From:
+        <br/>
+        MASONGO PATRICK
+      </div>
+    </Col>
+
+ 
+      {renderDetails()}
+
+</Row>
+
+<Row gutter={[16,16]}>
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Description</div>
+
+</Col>
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Programmes</div>
+
+</Col>
+
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Activity</div>
+
+</Col>
+
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Amount</div>
+
+</Col>
+
+  </Row>,
+
+  <Row gutter={[16,16]}>
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>RENTALS FROM HOUSES</div>
+
+</Col>
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>ECZ expenditure</div>
+
+</Col>
+
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Training</div>
+
+</Col>
+
+<Col span={6}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>800.00</div>
+
+</Col>
+
+  </Row>,
+<Col span={24}>
+<div style={{backgroundColor:'blue',height:50,marginTop:-25,fontWeight:'bold' }}>TOTAL</div>
+</Col>,
+<Col span={24}>
+<div style={{backgroundColor:'blue',height:50,marginTop:-15, }}>Amount in words</div>
+</Col>,
+
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Prepared By</div>
+
+</Col>
+
+<Col span={12}>
+<div style={{backgroundColor:'blue',height:50,margin:10,fontWeight:'bold'}}>Received By</div>
+
+</Col>
+
+
+      </>
+
+
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
     export default Receipt;
